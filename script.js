bd5dfa3df54ed9536dbf2f4f8aae1e6d2d295d8e
Pedro Santos <pedroaugustocostasantos98@gmail.com>
2024-09-26 11:15:04 -0300

    form integrado com a tabela do google

--- script.js
+++ script.js
@@ -62,3 +62,40 @@ document.getElementById('header__menuButton').addEventListener('click', function
     const header = document.querySelector('.header');
     header.classList.toggle('hidden');
 });
+
+// EndPoint do Formulario quintaPagina
+const handleSubmit = (event) => {
+    event.preventDefault();
+    addloading();
+    const name = document.querySelector('input[name=name]').value;
+    const idade = document.querySelector('input[name=idade]').value;
+    const sexo = document.querySelector('select[name=sexo').value;
+    const tel = document.querySelector('input[name=tel]').value;
+    const email = document.querySelector('input[name=email]').value;
+    const estado = document.querySelector('input[name=estado]').value;
+    const cidade = document.querySelector('input[name=cidade]').value;
+    const bairro = document.querySelector('input[name=bairro]').value;
+    const rua = document.querySelector('input[name=rua]').value;
+    const n = document.querySelector('input[name=n]').value;
+
+    fetch('https://api.sheetmonkey.io/form/c8b6qiAGB3D7Fj5koimMtp', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name, idade, sexo, tel, email, estado, cidade, bairro, rua, n}),
+    }).then(() => removeloading());
+}
+
+document.querySelector('.quintaPagina__form').addEventListener('submit', handleSubmit);
+
+// Animacao da imagem do form
+const button = document.querySelector('.enviarButton');
+const addloading = () => {
+    button.innerHTML = '<img src="./img/imgLoading.png" class="loading">';
+}
+
+const removeloading = () => {
+    button.innerHTML = 'Enviar';
+}
